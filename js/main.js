b8e3b12dee393503661ebbd065a66ad9e47ad7a5
Redouane Khiter <red.khiter.pro@gmail.com>
2021-03-10 16:26:49 +0100

    Feat : Upgrade content + images and added CV

--- js/main.js
+++ js/main.js
@@ -281,7 +281,7 @@
 
 				$.ajax({   	
 			      type: "POST",
-			      url: "inc/sendEmail.php",
+			      url: "https://formspree.io/f/xvovekqo",
 			      data: $(form).serialize(),
 
 			      beforeSend: function() { 
@@ -289,18 +289,10 @@
 			      },
 			      success: function(msg) {
 		            // Message was sent
-		            if (msg == 'OK') {
 		            	sLoader.fadeOut(); 
 		               $('#message-warning').hide();
 		               $('#contactForm').fadeOut();
 		               $('#message-success').fadeIn();   
-		            }
-		            // There was an error
-		            else {
-		            	sLoader.fadeOut(); 
-		               $('#message-warning').html(msg);
-			            $('#message-warning').fadeIn();
-		            }
 			      },
 			      error: function() {
 			      	sLoader.fadeOut(); 
